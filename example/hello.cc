851b420cfecc3b31d56a1ab9ac4e189b701b7949
cyliu <cyliu@localhost.localdomain>
2020-09-24 17:30:32 +0800

    fixed bug in hello.cc

--- example/hello.cc
+++ example/hello.cc
@@ -5,14 +5,18 @@
 #include "argparse.hpp"
 
 int main(int argc, char* argv[]) {
-    argparse::ArgumentParser parser;
-    parser.add_argument("--say", "welcome to use argparse!"); 
-    parser.add_argument("--verbose", "true"); 
-    std::map<std::string, std::string> args = parser.parse_args(argc, argv);
+    
     if (argc == 1) { 
         std::cout << "usage: ./hello --say \"hello,world\"" << std::endl;
+        exit(-1);
     }
-    else {
+
+    argparse::ArgumentParser parser;
+    parser.add_argument("--say", "welcome to use argparse!"); 
+    parser.add_argument("--verbose", "false"); 
+    std::map<std::string, std::string> args = parser.parse_args(argc, argv);
+    
+    if (args["--verbose"] == "true") {
         parser.dump();
     }
     return 0; 
